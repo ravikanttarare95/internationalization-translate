927c97e921dadcb0e2f00f9845cfb14b92617b81
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-08-14 14:18:00 +0530

    MOdified Design

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,43 +1,44 @@
 import I18n from "./../utils/I18n";
 import MessageComponent from "./../components/MessageComponent";
-import {Github} from "lucide-react";
+import { Github, Globe } from "lucide-react";
 
 function Home() {
   const ageCount = 24;
   const lang = localStorage.getItem("lang");
 
   return (
-    <div className="min-h-[100dvh] bg-gradient-to-br from-sky-200 via-blue-100 to-emerald-100 flex flex-col items-center px-5">
-      <div className="bg-white/70 backdrop-blur-lg shadow-2xl rounded-3xl w-full max-w-[600px] p-5 sm:p-7 md:p-9 border border-white/40 transition-all duration-300 hover:shadow-blue-200 mt-4 mb-18 sm:mt-8">
+    <div className="min-h-screen bg-gradient-to-br from-sky-200 via-blue-100 to-emerald-100 flex flex-col items-center px-5">
+      <div className="mb-6 sm:mb-8 fixed top-2 right-2 z-20 flex items-center gap-2">
+        <label
+          htmlFor="select-lang"
+          className="text-base sm:text-lg font-semibold text-emerald-500 mb-2"
+        >
+          <Globe className="mt-2 w-8 md:w-10 h-8 md:h-10" />
+        </label>
+        <select
+          name="select-lang"
+          id="select-lang"
+          defaultValue={lang}
+          onChange={(e) => {
+            localStorage.setItem("lang", e.target.value);
+            window.location.reload();
+          }}
+          className="w-full border-2 border-emerald-500 rounded-xl px-1 sm:px-2 py-1 bg-white/80 text-emerald-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 transition-all shadow-inner font-medium text-base sm:text-lg"
+        >
+          <option value="en">English</option>
+          <option value="mr">मराठी</option>
+          <option value="hi">हिंदी</option>
+        </select>
+      </div>
+
+      <div className="bg-white/70 backdrop-blur-lg shadow-2xl rounded-3xl w-full max-w-[600px] p-5 sm:p-7 md:p-9 border border-white/40 transition-all duration-300 hover:shadow-blue-200 mb-18 mt-20">
         <h1 className="text-2xl sm:text-3xl md:text-4xl font-extrabold text-center bg-gradient-to-r from-blue-700 via-sky-500 to-emerald-600 bg-clip-text text-transparent mb-6 sm:mb-8 drop-shadow-lg">
           Internationalization (I18n)
         </h1>
 
-        <div className="mb-6 sm:mb-8">
-          <label
-            htmlFor="select-lang"
-            className="block text-base sm:text-lg font-semibold text-blue-700 mb-2"
-          >
-            Choose Your Language:
-          </label>
-          <select
-            name="select-lang"
-            id="select-lang"
-            defaultValue={lang}
-            onChange={(e) => {
-              localStorage.setItem("lang", e.target.value);
-              window.location.reload();
-            }}
-            className="w-full border-2 border-emerald-300 rounded-xl px-3 py-3 sm:px-4 sm:py-4 bg-white/80 text-emerald-800 focus:outline-none focus:ring-4 focus:ring-emerald-300 transition-all shadow-inner font-medium text-base sm:text-lg"
-          >
-            <option value="en">English</option>
-            <option value="mr">Marathi</option>
-            <option value="hi">Hindi</option>
-          </select>
-        </div>
-
         <div className="space-y-3 sm:space-y-4">
           <MessageComponent messageFuncCall={I18n("greetingMessage")} />
+          <MessageComponent messageFuncCall={I18n("nameMessage")} />
           <MessageComponent messageFuncCall={I18n("ageMessage", ageCount)} />
           <MessageComponent messageFuncCall={I18n("cityMessage")} />
           <MessageComponent messageFuncCall={I18n("learningMessage")} />
